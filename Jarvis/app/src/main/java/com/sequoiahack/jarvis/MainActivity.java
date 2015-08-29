890480e5db5c6f9f4bcb63ddde60ed47e61227f1
VenomVendor <info@VenomVendor.com>
2015-08-29 15:34:42 +0530

    Setting up base modules. Changed package name.

--- Jarvis/app/src/main/java/com/sequoiahack/jarvis/MainActivity.java
+++ Jarvis/app/src/main/java/com/sequoiahack/jarvis/MainActivity.java
@@ -1,7 +1,7 @@
-package com.rahulaswani.jarvis;
+package com.sequoiahack.jarvis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
